ace7537d1dcfb382d0e11715d69fb4db3be42b12
igorstojanovic91 <i.stojanovic01@gmail.com>
2021-03-03 15:39:43 +0100

    adding statistics template to view

--- src/main/webapp/js/components/courses.js
+++ src/main/webapp/js/components/courses.js
@@ -8,24 +8,48 @@ export default {
     },
 
     render: function (moduleId) {
-        let $view = $($('#tpl-courses').html());
+        let $statistics = $($('#tpl-course-statistics').html());
+        let $table = $($('#tpl-courses').html());
+        let $view = $statistics.add($table)
         if (!moduleId) {
             return $('<p>Invalid parameter! Expecting moduleId.</p>'); //TODO: define error class
         }
         const module = store.getModule(moduleId);
         if (module) {
-            $('[data-field=title]').html(module.name);
+            $('[data-field=title]').empty()
+            $('[data-field=module-title]', $view).html(module.name)
         } else {
             router.go('/modules');
         }
-
         service.getModulesOverall(store.getUser(), moduleId)
             .then(data => initView($view, data))
         return $view;
     }
 };
 
+function initStatics($view, data) {
+    console.log($view)
+    $('[data-field=students-counter]', $view).html(data.length)
+    let bestGrade = 0;
+    let worstGrade = 100;
+    let gradeCounter = 0;
+    let passedStudents = 0;
+    data.forEach(student => {
+        if(student.overallGrade > bestGrade) bestGrade = student.overallGrade;
+        if(student.overallGrade < worstGrade) worstGrade = student.overallGrade;
+        if(student.overallGrade >= 50) passedStudents++;
+        console.log(student.overallGrade)
+        gradeCounter+=student.overallGrade;
+    })
+    $('[data-field=students-passed]', $view).html(passedStudents) // TODO
+    $('[data-field=average-grade]', $view).html(gradeCounter / data.length) // TODO
+    $('[data-field=best-grade]', $view).html(bestGrade) // TODO
+    $('[data-field=worst-grade]', $view).html(worstGrade) // TODO
+}
+
 function initView($view, data) {
+    console.log($view)
+    initStatics($($view[0]), data);
     const firstElement = data[0];
     createHeader($view, firstElement.courseRating);
     data.forEach(item => {
