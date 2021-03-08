2547bd61d8db2815e1f55961da04d87996e893db
igorstojanovic91 <i.stojanovic01@gmail.com>
2021-03-08 16:31:41 +0100

    fixing module overview query

--- src/main/java/ch/bfh/cassd2021/gruppe1/equals/repository/ModuleRepository.java
+++ src/main/java/ch/bfh/cassd2021/gruppe1/equals/repository/ModuleRepository.java
@@ -73,15 +73,13 @@ public class ModuleRepository {
         logger.debug("Entering getSuccessRateOverviewForModule()...");
         List<StudentCourseRating> studentCourseRatingList = new ArrayList<>();
 
-        String query = "SELECT re.studentID, p.lastName, p.firstName,"
-            + " c.id as courseId, c.name, c.shortName, c.moduleId, c.professorId, c.weight,"
-            + " ra.successRate, ra.version"
+        String query = "SELECT DISTINCT re.studentID, p.lastName, p.firstName, c.id as courseId, c.name, c.shortName, c.moduleId, c.professorId, c.weight, ra.successRate, ra.version"
             + " FROM Registration re"
-            + " INNER JOIN Person p ON re.studentId = p.id"
-            + " INNER JOIN Course c ON re.moduleId = c.moduleId"
-            + " LEFT JOIN Rating ra ON re.studentId = ra.studentId"
-            + " LEFT JOIN Module m ON c.moduleId = m.id"
-            + " WHERE re.moduleId = ?"
+            + " INNER JOIN Module m on re.moduleId = m.id"
+            + " INNER JOIN Course c on c.moduleId = m.id"
+            + " LEFT JOIN Rating ra on ra.courseId = c.id AND re.studentId = ra.studentId"
+            + " INNER JOIN Person p on p.id = re.studentId"
+            + " WHERE m.id = ?"
             + " AND (m.headId = ? OR m.assistantId = ? OR c.professorId = ? OR re.studentId = ?)"
             + " ORDER BY p.lastName asc, p.firstName asc, re.studentId asc, c.id asc";
 
