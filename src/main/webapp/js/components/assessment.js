d413f89a3f04d2b147e0bd318cb71a47307fe22f
Raphael Gerber <mail@raphaelgerber.com>
2021-03-16 14:29:56 +0100

    Displaying student data & ECTS grade in assessment view

--- src/main/webapp/js/components/assessment.js
+++ src/main/webapp/js/components/assessment.js
@@ -53,7 +53,7 @@ function initView($view, data) {
         tr.append(`<td class="has-text-left">${item.course.name}</td>`);
         tr.append(`<td>${item.course.weight}</td>`);
         tr.append(`<td>${item.rating.successRate}</td>`);
-        $('tbody', $view).append(tr);
+        $('tbody:last', $view).append(tr);
 
         if (Number(item.rating.successRate) > 0) {
             preliminaryWeight += Number(item.course.weight);
@@ -67,4 +67,30 @@ function initView($view, data) {
 
     $('[data-field=prelim-grade]', $view).html(finalPreliminaryGrade);
     $('[data-field=overall-grade]', $view).html(finalOverallGrade);
+
+    let ectsGrade = '';
+
+    if (finalPreliminaryGrade === finalOverallGrade && finalOverallGrade !== 0) {
+        if (finalOverallGrade >= 90) {
+            ectsGrade = 'A';
+        } else if (finalOverallGrade >= 80) {
+            ectsGrade = 'B';
+        } else if (finalOverallGrade >= 70) {
+            ectsGrade = 'C';
+        } else if (finalOverallGrade >= 60) {
+            ectsGrade = 'D';
+        } else if (finalOverallGrade >= 50) {
+            ectsGrade = 'E';
+        } else {
+            ectsGrade = 'F';
+        }
+    }
+
+    service.getPerson(store.getUser())
+        .then(data => {
+            $('[data-field=student-name]', $view).html(`${data.firstName} ${data.lastName} (${data.userName})`);
+            $('[data-field=date-of-birth]', $view).html(data.dateOfBirth.reverse().join('.'));
+            $('[data-field=place-of-origin]', $view).html(data.placeOfOrigin);
+            $('[data-field=ects-grade]', $view).html(ectsGrade);
+        })
 }
